bcacf139cf74e46e34bb6fadaf0ef5449d152e15
Denisse <denissep.orellana@gmail.com>
2022-02-12 01:59:04 -0300

    add: follow store ajax

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,22 +1,20 @@
 class FollowsController < ApplicationController
   def create
-    @store = Store.find(params[:store_id])
+    @store = Store.find_by(params[:store_id])
     current_user.stores << @store
+    # @follow = Follow.create!(user_id: current_user.id, store_id: @store.id)
     respond_to do |format|
       format.js { render nothing: false }
+      flash.now[:notice] = "Siguiendo a la tienda"
     end
-  end
-
-  def show
-    @store = Store.find(params[:store_id])
-    @follow = Follow.where(store_id: @store)
   end
 
   def destroy
     @store = current_user.follows.find_by(params[:store_id])
     @store.destroy
     respond_to do |format|
-        format.js { render nothing: false }
+      format.js { render nothing: false }
+      flash.now[:notice] = "Has dejado de seguir la tienda"
     end
   end
 end
